0fe01c6812adadefafe9b8828f1c73eabbcdf049
IIIgorMoura <133662971+IIIgorMoura@users.noreply.github.com>
2026-04-27 19:21:23 -0300

    add tratamento para boolean

--- 1_58_probada_dados.py
+++ 1_58_probada_dados.py
@@ -6,6 +6,9 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Any,Dict, List, Optional, Tuple
 
+
+# tratamento: Conversão para Float
+
 def to_float(value: Any) -> Optional[float]:
     # value: Any: aceita qualquer tipo de valor.
     # -> Optional[float]: pode retornar um float ou None (caso não consiga converter).
@@ -23,4 +26,35 @@ def to_float(value: Any) -> Optional[float]:
         except ValueError:
             return None
             
+    return None
+
+
+
+# teste=to_float("R$ 45,99")
+# print(teste)
+
+# Recebe qualquer tipo (Any).
+# Retorna True, False ou None.
+def to_bool(value: Any) -> Optional[bool]:
+    if value is None or value == "":
+        return None
+    
+    if isinstance(value, bool):  # Se já for booleano (True ou False), retorna direto.
+        return value
+
+    # Converte números: 0 -> False, qualquer outro número -> True
+    if isinstance(value, (int, float)):
+        return bool(value)
+
+    if isinstance(value, str):  # Se for string: remove espaços (strip) e converte
+        text = value.strip().lower()
+        
+        if text in {"true", "1", "sim", "s", "yes", "y"}:
+            return True
+            
+        if text in {"false", "0", "nao", "não", "n", "no"}:
+            return False
+
+        # Isso facilita comparar valores como "Sim", " SIM " etc.
+        
     return None
